4b74b20dff496444760fc394b66ee3b1361e4fce
JacquelineCastro <jcastr30@ucsc.edu>
2022-05-23 23:03:13 -0700

    savingProgress
    might have a couple things broken but the code is somewhat simplified

---
+++ js/testCodeSave.js
@@ -0,0 +1,90 @@
+//-----KINDA WORKING CODE. SO THIS CODE REMOVES THE NUMBER FRM THE LIST-------//
+/*
+  deathButtEl.click(function(){
+    val = Math.floor(Math.random() * cardLeft.length);
+    console.log("val: "+val+" & cardLeft: "+ cardLeft);
+    switchContainer(val);
+  })
+  nextButtEl.click(function(){
+    if (val == 0){
+      val = Math.floor(Math.random() * cardLeft.length);
+      console.log("val: "+val+" & cardLeft: "+cardLeft);
+      switchContainer(val);
+    } else {
+      cardLeft.splice(val-1, 1);
+      val = Math.floor(Math.random() * cardLeft.length);
+      switchContainer(val);
+      console.log("val: "+val+" & cardLeft: "+cardLeft);
+    }
+
+    console.log(cardLeft);
+  })
+*/
+
+//-------------------------------SWITCHES SAVED-------------------------------//
+/*
+function switchContainer(val){
+  switch (val){
+    case 0:
+      outputEl.html("<img src='../img/placeholder/cards/CardInfo.png' class='profiles'/>");
+      console.log("Image 0");
+      break;
+    case 1:
+      outputEl.html("<img src='../img/placeholder/cards/card1.png' class='profiles'/>");
+      console.log("Image 1");
+      break;
+    case 2:
+      outputEl.html("<img src='../img/placeholder/cards/card2.png' class='profiles'/>");
+      console.log("Image 2");
+      break;
+    case 3:
+      outputEl.html("<img src='../img/placeholder/cards/card3.png' class='profiles'/>");
+      console.log("Image 3");
+      break;
+    case 4:
+      outputEl.html("<img src='../img/placeholder/cards/card4.png' class='profiles'/>");
+      console.log("Image 4");
+      break;
+    case 5:
+      outputEl.html("<img src='../img/placeholder/cards/card5.png' class='profiles'/>");
+      console.log("image 5");
+      break;
+    case 6:
+      outputEl.html("<img src='../img/placeholder/cards/card6.png' class='profiles'/>");
+      console.log("image 6");
+      break;
+    case 7:
+      outputEl.html("<img src='../img/placeholder/cards/card7.png' class='profiles'/>");
+      console.log("image 7");
+      break;
+    case 8:
+      outputEl.html("<img src='../img/placeholder/cards/card8.png' class='profiles'/>");
+      console.log("image 8");
+      break;
+    case 9:
+      outputEl.html("<img src='../img/placeholder/cards/card9.png' class='profiles'/>");
+      console.log("image 9");
+      break;
+    case 10:
+      outputEl.html("<img src='../img/placeholder/cards/card10.png' class='profiles'/>");
+      console.log("image 10");
+      break;
+    case 11:
+      outputEl.html("<img src='../img/placeholder/cards/card11.png' class='profiles'/>");
+      console.log("image 11");
+      break;
+    case 12:
+      outputEl.html("<img src='../img/placeholder/cards/card12.png' class='profiles'/>");
+      console.log("image 12");
+      break;
+    case 13:
+      outputEl.html("<img src='../img/placeholder/cards/card13.png' class='profiles'/>");
+      console.log("image 13");
+      break;
+    case 14:
+      outputEl.html("<img src='../img/placeholder/cards/card14.png' class='profiles'/>");
+      console.log("Image 14");
+      break;
+  }
+}
+*/
--- js/tinderPage.js
+++ js/tinderPage.js
@@ -1,66 +1,114 @@
 /*
 Here is the main Javascript for the slidshow of images.
 */
-function nextSlide(x){
-  var val = Math.floor(Math.random() * x);
+function switchContainer(val){
 
   switch (val){
     case 0:
-      outputEl.html("<img src='../img/placeholder/cards/CardInfo.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/CardInfo.png' class='profiles' />");
+      console.log("Image 0");
       break;
     case 1:
-      outputEl.html("<img src='../img/placeholder/cards/card1.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card1.png' class='profiles' />");
+      console.log("Image 1");
       break;
     case 2:
-      outputEl.html("<img src='../img/placeholder/cards/card2.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card2.png' class='profiles' />");
+      console.log("Image 2");
       break;
     case 3:
-      outputEl.html("<img src='../img/placeholder/cards/card3.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card3.png' class='profiles' />");
+      console.log("Image 3");
       break;
     case 4:
-      outputEl.html("<img src='../img/placeholder/cards/card4.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card4.png' class='profiles' />");
+      console.log("Image 4");
       break;
     case 5:
-      outputEl.html("<img src='../img/placeholder/cards/card5.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card5.png' class='profiles' />");
+      console.log("image 5");
       break;
     case 6:
-      outputEl.html("<img src='../img/placeholder/cards/card6.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card6.png' class='profiles' />");
+      console.log("image 6");
       break;
     case 7:
-      outputEl.html("<img src='../img/placeholder/cards/card7.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card7.png' class='profiles' />");
+      console.log("image 7");
       break;
     case 8:
-      outputEl.html("<img src='../img/placeholder/cards/card8.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card8.png' class='profiles' />");
+      console.log("image 8");
       break;
     case 9:
-      outputEl.html("<img src='../img/placeholder/cards/card9.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card9.png' class='profiles' />");
+      console.log("image 9");
       break;
     case 10:
-      outputEl.html("<img src='../img/placeholder/cards/card10.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card10.png' class='profiles' />");
+      console.log("image 10");
       break;
     case 11:
-      outputEl.html("<img src='../img/placeholder/cards/card11.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card11.png' class='profiles' />");
+      console.log("image 11");
       break;
     case 12:
-      outputEl.html("<img src='../img/placeholder/cards/card12.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card12.png' class='profiles' />");
+      console.log("image 12");
       break;
     case 13:
-      outputEl.html("<img src='../img/placeholder/cards/card13.png' class='profiles'/>");
+      outputEl.html("<img src='../img/placeholder/cards/card13.png' class='profiles' />");
+      console.log("image 13");
       break;
     case 14:
-      outputEl.html("<img src='../img/placeholder/cards/card14.png' class='profiles'/>");
-      break;
+      outputEl.html("<img src='../img/placeholder/cards/card14.png' class='profiles' />");
+      console.log("Image 14");
+  }
+
+}
+function shuffleCard(x, statment){
+  let val = Math.floor(Math.random() * x);
+  switchContainer(val);
+
+  if(statment == true){
+    fishInfo[round] = val;
+    console.log(fishInfo);
+    round++;
   }
 }
 
-outputEl = $("#output");
+//Start Here !
+outputEl    = $("#output");                            //Collect the output location
 
-deathButtEl = $("#deathButt");
-nextButtEl = $("#nextButt");
+deathButtEl = $("#deathButt");                         //Collect Button information
+nextButtEl  = $("#nextButt");
+
+dmEl        = $("#dm");
+matchesEl   = $("#matches");
+listEl      = $("#list");
+creditsEl   = $("#credits");
+
+profiles  = $(".profiles");
 
 var cardNum = 14;
-outputEl.html(nextSlide(0));
+switchContainer(0);
+let fishInfo = [];
+let round = 0;
+let hidden = false;
 
+//When These button are Clicked, shuffle the cards and check if liked or disliked
 deathButtEl.click(function(){
-  outputEl.html(nextSlide(cardNum));
-});
+  shuffleCard(cardNum, false);
+})
+nextButtEl.click(function(){
+  shuffleCard(cardNum, true);
+})
+
+matchesEl.click(function(){
+  console.log("click");
+  if(profiles.css("visibility") == "hidden"){
+    profiles.css("visibility", "visible");
+  } else {
+    profiles.css("visibility", "hidden");
+  }
+})
