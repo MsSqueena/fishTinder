547b5d62e9b5445ee751d966a87f7363a61ffcfc
JacquelineCastro <jcastr30@ucsc.edu>
2022-05-31 16:00:09 -0700

    Clean Cards
    Fixed the function that shuffle cards.
    - No repeat card next to each other
    - added p5.js & p5.play.js
    - properly collects the liked cards
    - added slide animation to cards
    - tried to add more comments

--- js/tinderPage.js
+++ js/tinderPage.js
@@ -1,83 +1,107 @@
 /*
 Here is the main Javascript for the slidshow of images.
 */
-function switchContainer(val){
+
+//----------------------FUNCTIONS---------------------------------------------//
+function switchContainer(val){        //Holds all the variations of fish and their fish cards!
 
   switch (val){
-    case 0:
-      outputEl.html("<img src='../img/placeholder/cards/CardInfo.png' class='profiles' />");
+    case 0:                   //INFORMATION card
+      outputEl.html("<img src='../img/placeholder/cards/CardInfo.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("Image 0");
       break;
-    case 1:
-      outputEl.html("<img src='../img/placeholder/cards/card1.png' class='profiles' />");
+    case 1:                   //CATFISH
+      outputEl.html("<img src='../img/placeholder/cards/card1.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("Image 1");
       break;
-    case 2:
-      outputEl.html("<img src='../img/placeholder/cards/card2.png' class='profiles' />");
+    case 2:                   //OARFISH
+      outputEl.html("<img src='../img/placeholder/cards/card2.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("Image 2");
       break;
-    case 3:
-      outputEl.html("<img src='../img/placeholder/cards/card3.png' class='profiles' />");
+    case 3:                   //CLOWNFISH
+      outputEl.html("<img src='../img/placeholder/cards/card3.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("Image 3");
       break;
-    case 4:
-      outputEl.html("<img src='../img/placeholder/cards/card4.png' class='profiles' />");
+    case 4:                   //ANGLERFISH
+      outputEl.html("<img src='../img/placeholder/cards/card4.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("Image 4");
       break;
-    case 5:
-      outputEl.html("<img src='../img/placeholder/cards/card5.png' class='profiles' />");
+    case 5:                   //SEAGULL
+      outputEl.html("<img src='../img/placeholder/cards/card5.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 5");
       break;
-    case 6:
-      outputEl.html("<img src='../img/placeholder/cards/card6.png' class='profiles' />");
+    case 6:                   //HAMMERHEAD
+      outputEl.html("<img src='../img/placeholder/cards/card6.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 6");
       break;
-    case 7:
-      outputEl.html("<img src='../img/placeholder/cards/card7.png' class='profiles' />");
+    case 7:                   //REMORA
+      outputEl.html("<img src='../img/placeholder/cards/card7.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 7");
       break;
-    case 8:
-      outputEl.html("<img src='../img/placeholder/cards/card8.png' class='profiles' />");
+    case 8:                   //PORCUPINEFISH
+      outputEl.html("<img src='../img/placeholder/cards/card8.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 8");
       break;
-    case 9:
-      outputEl.html("<img src='../img/placeholder/cards/card9.png' class='profiles' />");
+    case 9:                   //GOBLIN SHARK
+      outputEl.html("<img src='../img/placeholder/cards/card9.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 9");
       break;
-    case 10:
-      outputEl.html("<img src='../img/placeholder/cards/card10.png' class='profiles' />");
+    case 10:                   //EEL
+      outputEl.html("<img src='../img/placeholder/cards/card10.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 10");
       break;
-    case 11:
-      outputEl.html("<img src='../img/placeholder/cards/card11.png' class='profiles' />");
+    case 11:                   //PIRANA
+      outputEl.html("<img src='../img/placeholder/cards/card11.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 11");
       break;
-    case 12:
-      outputEl.html("<img src='../img/placeholder/cards/card12.png' class='profiles' />");
+    case 12:                   //FLOUNDER
+      outputEl.html("<img src='../img/placeholder/cards/card12.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 12");
       break;
-    case 13:
-      outputEl.html("<img src='../img/placeholder/cards/card13.png' class='profiles' />");
+    case 13:                   //MUDSKIPPER
+      outputEl.html("<img src='../img/placeholder/cards/card13.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("image 13");
       break;
-    case 14:
-      outputEl.html("<img src='../img/placeholder/cards/card14.png' class='profiles' />");
+    case 14:                   //BLOBFISH (SHOULD PROBABLY REMOVE HIM)
+      outputEl.html("<img src='../img/placeholder/cards/card14.png' id='profiles' class='w3-container w3-animate-right'/>");
       console.log("Image 14");
   }
-
 }
+//This function picks a random number and reveals the card.
 function shuffleCard(x, statment){
-  let val = Math.floor(Math.random() * x);
-  switchContainer(val);
-
-  if(statment == true){
-    fishInfo[round] = val;
+  //IF YOU LIKED THE FISH CARD, SAVE IT
+  if((statment == true)&&(prev != 0)){
+    fishInfo[round] = prev;
     console.log(fishInfo);
     round++;
+    console.log(round);
+  }
+
+//ELSE, LET'S SHUFFLE THE CARDS
+  let val = Math.floor(Math.random() * x);
+  while (val == 0){
+    val = Math.floor(Math.random() * x);
+  }
+//
+  if (val == prev){
+    if (val == 14){
+      val -= 1;
+      switchContainer(val);
+      console.log("prev");
+      prev = val;
+    } else {
+      val += 1;
+      switchContainer(val);
+      console.log("prev");
+      prev = val;
+    }
+  } else{
+    switchContainer(val);
+    prev = val;
   }
 }
 
-//Start Here !
+//------------------------WORKING CODE----------------------------------------//
 outputEl    = $("#output");                            //Collect the output location
 
 deathButtEl = $("#deathButt");                         //Collect Button information
@@ -88,15 +112,15 @@ matchesEl   = $("#matches");
 listEl      = $("#list");
 creditsEl   = $("#credits");
 
-profiles  = $(".profiles");
+var cardNum = 14;                                      //NUMBER OF CARDS
+switchContainer(0);                                    //START WITH THE INFO CARD
+let fishInfo = [];                                     //ARRAY FOR FISHES LIKED
+let round = 0;                                         //PLACEHOLDER FOR FISH VALUE
+let hidden = false;                                    //CARDS ARE NOT HIDDEN
+let prev = 0;
 
-var cardNum = 14;
-switchContainer(0);
-let fishInfo = [];
-let round = 0;
-let hidden = false;
-
-//When These button are Clicked, shuffle the cards and check if liked or disliked
+//------------------------BUTTONS & WHAT THEY D0------------------------------//
+//FishBONES & HEARTS button shuffle the fish cards shown
 deathButtEl.click(function(){
   shuffleCard(cardNum, false);
 })
@@ -104,11 +128,23 @@ nextButtEl.click(function(){
   shuffleCard(cardNum, true);
 })
 
+//MATCHES button hids the fishCards and the buttons on screen so you could see
+//everything swimming in the background.
 matchesEl.click(function(){
   console.log("click");
-  if(profiles.css("visibility") == "hidden"){
-    profiles.css("visibility", "visible");
+
+//This turns off the fishCards
+  if($("#profiles").css("visibility") == "hidden"){
+    $("#profiles").css("visibility", "visible");
   } else {
-    profiles.css("visibility", "hidden");
+    $("#profiles").css("visibility", "hidden");
+  }
+//this turns off the buttons
+  if($(".choiceButtons").css("visibility") == "hidden"){
+    $(".choiceButtons").css("visibility", "visible");
+  } else {
+    $(".choiceButtons").css("visibility", "hidden");
   }
 })
+
+//-----------------MOVING OBJECTS IN THE BACKGROUND---------------------------//
