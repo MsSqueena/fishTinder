be8d02e526be11409981ed99a2cc7732956352a1
JacquelineCastro <jcastr30@ucsc.edu>
2022-06-06 20:53:59 -0700

    added some assets
    When you hover over the buttons, it changes the saturation and hue, and added the images that I have at the moment

--- js/tinderPage.js
+++ js/tinderPage.js
@@ -4,100 +4,133 @@ Here is the main Javascript for the slidshow of images.
 
 //----------------------FUNCTIONS---------------------------------------------//
 let fishPrints = [
-  { //INFOMRATION CARD WITH ALL THE THINGS IN IT
+  { //INFOMRATION CARD          #0
     name:     "Infromation Card",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Sup fish<br>             Location: Everywhere and nowhere fam<br>"+
-              "Height: Who knows fam<br>      Age: Who knows fam",
+    fishInfo: "<b>Name:</b> Sup fish<br>             <b>Location:</b> Everywhere and nowhere fam<br>"+
+              "<b>Height:</b> Who knows fam<br>      <b>Age:</b> Who knows fam",
 
     fishBio:  "Hey, how's it going?<br>"+
               "Fish Tinder Here! Are you interested in finding your match? How about learning about a couple of fish? Here you can find all of that and more! "+
-              "To start, press either the fish bones button or the heart button."
+              "To start, press either the fish bones button or the heart button.",
   },
-  { //WELS CATIFISH
+  { //WELS CATIFISH             #1
     name:     "Silurus Glanis",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Wels Catfish <br>         Location: Eastern/Western Europe (Also in Spain & Italy) <br>"+
-              "Height: 15ft<br>                Age: 15 - 80yr",
+    fishInfo: "<b>Name:</b> Wels Catfish <br>         <b>Location:</b> Eastern/Western Europe (Also in Spain & Italy) <br>"+
+              "<b>Height:</b> 15ft<br>                <b>Age:</b> 15 - 80yr",
 
    fishBio:   "Obviously this fish wants to catfish you.<br>"+
               "How do people catfish?<br>"+
               "They could be pretending to be another kind of fish, coul dbe getting a really good camera angle, could have photoshopped in a hot human body, could have a filter. . ."+
               "It's important though that we don't make the punchline that the catfish is ugly, cause that's mean."
   },
-  { // OARFISH
+  { // OARFISH                   #2
     name:     "Regalecus Glesne",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Oarfish <br>               Location: Worldwide<br>"+
-              "Height: 36ft<br>                 Age: N/A",
+    fishInfo: "<b>Name:</b> Oarfish <br>               <b>Location:</b> Worldwide<br>"+
+              "<b>Height:</b> 36ft<br>                 <b>Age:</b> N/A",
 
     fishBio:  "Oarfish are rare. So two ways: <br>"+
               "Mysterious beauty. Like nobody can ever find them so they're super aloof and cool.<br>"+
               "Or like, nobody can ever find them cos they're shy.<br>"+
               "Oarfish have this huge plume of decorative 'feathers' on their head tho and homie defintely thinks it their best feature.",
   },
-  { //COMMON CLOWNFISH
+  { //COMMON CLOWNFISH            #3
     name:     "Amphiprion Ocellaris",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Common Clownfish <br>       Location: The Eastern Indian Ocean & Western Pacific<br>"+
-              "Height: 4.3in<br>                 Age: 10yrs",
+    fishInfo: "<b>Name:</b> Common Clownfish <br>       <b>Location:</b> The Eastern Indian Ocean & Western Pacific<br>"+
+              "<b>Height:</b> 4.3in<br>                 <b>Age:</b> 10yrs",
 
     fishBio:  "I mean... it's a clown fish so it should probably try and tell jokes in the chat...<br>"+
               "If not in the bio at least. It is the shorest fish we have here if we want to play with it's height."
   },
-  { //HUMPBACK ANGLERFISH
+  { //HUMPBACK ANGLERFISH         #4
     name:     "Ceratioidei",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Humpback Anglerfish <br>    Location: Antarctic & Atlantic Ocean <br>"+
-              "Height: Up To 4ft <br>            Age: 30yrs",
+    fishInfo: "<b>Name:</b> Humpback Anglerfish <br>    <b>Location:</b> Antarctic & Atlantic Ocean <br>"+
+              "<b>Height:</b> Up To 4ft <br>            <b>Age:</b> 30yrs",
 
     fishBio:  "Ugly cute? Maybe a goth/emo type dude? Maybe has that scary hello kitty dropping xans tattoo chains +"+
               " all black instagram asthetic"
-
   },
-  { //SEAGULL TRYING TO BE A FISH
+  { //SEAGULL TRYING TO BE A FISH   #5
     name:     "Realus Fishus",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Fihsh <br>                   Location: Wahther<br>"+
-              "Height: 2ft<br>                    Age: 5 - 15yr",
+    fishInfo: "<b>Name:</b> Fihsh <br>                   <b>Location:</b> Wahther<br>"+
+              "<b>Height:</b> 2ft<br>                    <b>Age:</b> 5 - 15yr",
 
     fishBio:  "Def wearing a big fish mascot suit. 'Hi! I like doing normal fish things like breathing water!"+
               "And going for a swim with my school! and eating fi--- eating seaweed!'",
   },
-  { //SMOOTH HAMMERHEAD SHARK
+  { //SMOOTH HAMMERHEAD SHARK       #6
     name:     "Sphyrna Zygaena",
     fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
 
-    fishInfo: "Name: Smooth Hammerhead Shark <br>       Location: Worldwide (Tropical & Temperate Waters) <br>"+
-              "Height: 4 Meters <br>                    Age: 20yrs",
+    fishInfo: "<b>Name:</b> Smooth Hammerhead Shark <br>       <b>Location:</b> Worldwide (Tropical & Temperate Waters) <br>"+
+              "<b>Height:</b> 4 Meters <br>                    <b>Age:</b> 20yrs",
 
-    fishBio:  "<div class='fishBio'>"+
-              "Really into its head shape. Flexes its 360 vision"+
-              "</div>",
+    fishBio:  "Really into its head shape. Flexes its 360 vision",
   },
-  {
-    name:     ""
+  { //REMORA                        #7
+    name:     "Echeneidae",
+    fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
+
+    fishInfo: "<b>Name:</b> Remora <br>                        <b>Location:</b> Tropical & Subtropical Oceans <br>"+
+              "<b>Height:</b> 30-90cm <br>                     <b>Age:</b> Unknown",
+
+    fishBio:  "Looking for Sharks, Sea Turtles and Whales. (Definitely a sugarbaby)"
+  },
+  { //PORCUPINEFISH                 #8
+    name:     "Diodontidae",
+    fishImg:  "<img src='../img/placeholder/bubble.png' class='fishImage'</div>",
+
+    fishInfo: "<b>Name:</b> Porcupinefish <br>                 <b>Location:</b> Tropical & Subtropical Environments <br>"+
+              "<b>Height</b>: 36 inches <br>                   <b>Age:</b> 10-15yrs",
+
+    fishBio:  "Play into the fact that it has a toxic substance covering it's skin"
+  },
+  { // GOBLIN SHARK                 #9
+    name:     "Mitsukurina Owstoni",
+    fishImg:  "<img src='../img/official/profileImage/GoblinShark.jpg' class='fishImage'</div>",
+
+    fishInfo: "<b>Name:</b> Goblin Shark <br>                  <b>Location:</b> Temperate & Tropical Waters <br>"+
+              "<b>Height:</b> 12ft. <br>                       <b>Age:</b> 60yrs",
+
+    fishBio:  "Maybe because we can see it's teeth all the time, we can make it seem like the Goblin Shark "+
+              " is really happy all the time or that it's trying to be intimidating all the time."
+  },
+  { //BLOBFISH                      #10
+    name:     "Psychrolutes Marcidus",
+    fishImg:  "<img src='../img/official/profileImage/Blobfish.jpg' class='fishImage'</div>",
+
+    fishInfo: "<b>Name:</b> Blobfish <br>                       <b>Location:</b> Atlantic, Indian & Pacific Ocean <br>"+
+              "<b>Height:</b> >30cm <br>                        <b>Age:</b> Up to 130",
+
+    fishBio:  "Refrence the Fact that people usually think it looks weird becuase when it comes to the surface "+
+              " it's usually dead. Something about it having very soft bones just makes me think it should be a pushover."
   }
 ]
-
+//----------------------------------------------------------------------------//
 
 function objContainer(val){
   let curFish = fishPrints[val];
 
   outputEl.html(curFish.fishImg);
   outputEl.append("<div class='fishBio'>" +curFish.fishBio+ "</div>");
-  outputEl.append("<div class='name'> " +curFish.name+ " </div>");
+  outputEl.append("<div class='name'> <b>" +curFish.name+ "</b> </div>");
   outputEl.append("<div class='fishInfo'>" +curFish.fishInfo+ "</div>");
 
 }
 //This function picks a random number and reveals the card.
 function shuffleCard(x, statment){
+
   //IF YOU LIKED THE FISH CARD, SAVE IT
   if((statment == true)&&(prev != 0)){
     fishInfo[round] = prev;
@@ -141,8 +174,8 @@ matchesEl   = $("#matches");
 listEl      = $("#list");
 creditsEl   = $("#credits");
 
-var cardNum = 4;                                      //NUMBER OF CARDS
-objContainer(0);                                    //START WITH THE INFO CARD
+var cardNum = 11;                                      //NUMBER OF CARDS
+objContainer(0);                                       //START WITH THE INFO CARD
 let fishInfo = [];                                     //ARRAY FOR FISHES LIKED
 let round = 0;                                         //PLACEHOLDER FOR FISH VALUE
 let hidden = false;                                    //CARDS ARE NOT HIDDEN
@@ -151,27 +184,29 @@ let prev = 0;
 //------------------------BUTTONS & WHAT THEY D0------------------------------//
 //FishBONES & HEARTS button shuffle the fish cards shown
 deathButtEl.click(function(){
-  outputEl.removeClass("w3-container w3-animate-right");
-  outputEl.addClass("w3-container w3-animate-right");
   shuffleCard(cardNum, false);
 })
+deathButtEl.hover(function(){
+  deathButtEl.toggleClass("deathButtTwo");
+})
 nextButtEl.click(function(){
-  outputEl.removeClass("w3-container w3-animate-right");
-  outputEl.addClass("w3-container w3-animate-right");
   shuffleCard(cardNum, true);
 })
+nextButtEl.hover(function(){
+  nextButtEl.toggleClass("nextButtTwo");
+})
+
 //MATCHES button hids the fishCards and the buttons on screen so you could see
 //everything swimming in the background.
 matchesEl.click(function(){
   console.log("click");
-
-//This turns off the fishCards
-  if($("#profiles").css("visibility") == "hidden"){
-    $("#profiles").css("visibility", "visible");
+  //This turns off the fishCards
+  if($("#output").css("visibility") == "hidden"){
+    $("#output").css("visibility", "visible");
   } else {
-    $("#profiles").css("visibility", "hidden");
+    $("#output").css("visibility", "hidden");
   }
-//this turns off the buttons
+  //this turns off the buttons
   if($(".choiceButtons").css("visibility") == "hidden"){
     $(".choiceButtons").css("visibility", "visible");
   } else {
